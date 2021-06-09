6938da0008ffeca64588f6f94c8f9212631b2546
Daniel Chen <72116969+dannychen0830@users.noreply.github.com>
2021-06-09 17:06:44 -0400

    Add files via upload
    - made compatible without the command-line arguments

--- NES/NES_energy.py
+++ NES/NES_energy.py
@@ -32,3 +32,33 @@ def MIS_energy(cf, adj):
             if J[i, j] != 0.:
                 ha += J[i, j] * nk.operator.LocalOperator(hi, [np.kron(b, b)], [[i, j]])
     return ha, g, hi
+
+def MIS_energy(adj, penalty):
+    # construct the MIS hamiltonian
+    J = penalty*adj - np.eye(adj.shape[0])
+
+    # define tools:
+    # size of the graph
+    N = J.shape[0]
+    # operators on each site
+    sz = np.array([[1., 0.], [0., -1.]])
+    id = np.array([[1., 0.], [0., 1.]])
+    b = (id - sz) / 2
+
+    # create graph
+    edges = []
+    for i in range(N):
+        for j in range(i, N):
+            if J[i, j] != 0.: edges.append([i, j])
+    g = nk.graph.CustomGraph(edges)
+
+    # system with spin-1/2 particles
+    hi = nk.hilbert.Spin(s=0.5, graph=g)
+    ha = nk.operator.LocalOperator(hi, 0)
+
+    # construct the hamiltonian
+    for i in range(N):
+        for j in range(N):
+            if J[i, j] != 0.:
+                ha += J[i, j] * nk.operator.LocalOperator(hi, [np.kron(b, b)], [[i, j]])
+    return ha, g, hi
