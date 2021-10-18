ab9341e6e2180029591dc113ae399be3b5c02709
Daniel Chen <72116969+dannychen0830@users.noreply.github.com>
2021-10-18 12:38:38 -0400

    Add files via upload

--- NES/NES_energy.py
+++ NES/NES_energy.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import netket as nk
 
@@ -16,16 +18,24 @@ def MIS_energy(cf, adj):
     b = (id - sz) / 2
 
     # create graph
+    t1 = time.time()
     edges = []
     for i in range(N):
         for j in range(i+1, N):
             if J[i, j] != 0.:
                 edges.append([i, j])
+    t2 = time.time()
+    # print('edges:', t2-t1)
     # g = nk.graph.CustomGraph(edges)
     g = nk.graph.Graph(edges=edges)
+    t1 = time.time()
+    print('graph construction:', t1 - t2)
+
 
     # system with spin-1/2 particles
     hi = nk.hilbert.Spin(s=0.5, N=g.n_nodes)
+    t2 = time.time()
+    # print('hilbert: ', t2 - t1)
     # hi = nk.hilbert.CustomHilbert(local_states=[-1,1], N=cf.input_size)
     # ha = nk.operator.LocalOperator(hi, 0)
 
@@ -35,7 +45,14 @@ def MIS_energy(cf, adj):
     #         if J[i, j] != 0.:
     #             ha += J[i, j] * nk.operator.LocalOperator(hi, [np.kron(b, b)], [[i, j]])
 
-    ha = nk.operator.GraphOperator(hilbert=hi, graph=g, site_ops=[-1*b], bond_ops=[cf.penalty*np.kron(b, b)])
+    L = cf.penalty*np.kron(b, b)
+    # ha = nk.operator.GraphOperator(hilbert=hi, graph=g, site_ops=[-1*b], bond_ops=[L])
+    t1 = time.time()
+    # print('hamiltonian:', t1-t2)
+
+    t = time.time()
+    ha = nk.operator.Ising(h=0, hilbert=hi, J=cf.penalty, graph=g)
+    print('ising', time.time() - t)
     return ha, g, hi
 
 
@@ -55,6 +72,24 @@ def Maxcut_energy(cf, adj):
 
     # system with spin-1/2 particles
     hi = nk.hilbert.Spin(s=0.5, N=g.n_nodes)
-    ha = nk.operator.GraphOperator(hilbert=hi, graph=g, bond_ops=[cf.penalty*np.kron(sz, sz)])
+    ha = nk.operator.GraphOperator(hilbert=hi, graph=g, bond_ops=[np.kron(sz, sz)])
     return ha, g, hi
 
+
+def Transverse_Ising_Energy(cf, adj):
+    N = adj.shape[0]
+    z = np.array([[1, 0],[0, -1]])
+    x = np.array([[0, 1],[1, 0]])
+
+    edges = []
+    for i in range(N):
+        for j in range(i + 1, N):
+            if adj[i, j] != 0.:
+                edges.append([i, j])
+
+    g = nk.graph.Graph(edges=edges)
+
+    # system with spin-1/2 particles
+    hi = nk.hilbert.Spin(s=0.5, N=g.n_nodes)
+    ha = nk.operator.GraphOperator(hilbert=hi, graph=g, site_ops=[-1*x], bond_ops=[np.kron(z,z)])
+    return ha, g, hi
